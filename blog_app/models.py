1cea8cbfa973bfc11552e765320f3e0f0fd555a2
Felipe Costa <felipecosta53@info.ufrn.br>
2023-02-26 17:25:35 -0300

    blog_app models broken import fixed

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 from django.utils import timezone
-from django.core.urls import reverse
+from django.urls import reverse
 
 # Create your models here.
 
 
 class Post(models.Model):
-    author = models.ForeignKey('auth.User')
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
-    create_date = models.DateTimeField(default=timezone.now())
+    create_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
@@ -27,10 +27,11 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    post = models.ForeignKey('blog.Post', related_name='comments')
+    post = models.ForeignKey(
+        'blog_app.Post', related_name='comments', on_delete=models.CASCADE)
     author = models.CharField(max_length=200)
     text = models.TextField()
-    create_date = models.DateTimeField(default=timezone.now())
+    create_date = models.DateTimeField(default=timezone.now)
     approved_comment = models.BooleanField(default=False)
 
     def approve(self):
