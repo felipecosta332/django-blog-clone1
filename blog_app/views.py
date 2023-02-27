c9c0a7db3a8cf2b86dd7fca341fe130a9f988ea8
Felipe Costa <felipecosta53@info.ufrn.br>
2023-02-27 10:23:18 -0300

    creates CRUD views and urls for Post

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,8 +1,58 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.utils import timezone
 from django.views.generic import TemplateView
+from django.views.generic import ListView
+from django.views.generic import DetailView
+from django.views.generic import CreateView
+from django.views.generic import UpdateView
+from django.views.generic import DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from blog_app.models import Post
+from blog_app.models import Comment
+from blog_app.forms import PostForm
 
 # Create your views here.
 
 
 class AboutView(TemplateView):
     template_name = 'about.html'
+
+
+class PostListView(ListView):
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class CreatePostView(LoginRequiredMixin, CreateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog_app/post_detail.html'
+    form_class = PostForm
+    model = Post
+
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    login_url = '/login/'
+    redirect_field_name = 'blog_app/post_detail.html'
+    form_class = PostForm
+    model = Post
+
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = Post
+    success_url = reverse_lazy('post_list')
+
+
+class DraftListView(LoginRequiredMixin, ListView):
+    login_url = '/login/'
+    redirect_field_name = 'blog_app/post_list.html'
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__isnull=True).order_by('created_date')
